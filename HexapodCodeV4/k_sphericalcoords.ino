6df44de6b0f9edf80252d6268e2cc6eae0e30d9f
ak182182 <Ak182182@gmail.com>
2019-03-08 07:43:50 -0500

    partial sphericalcoords

--- HexapodCodeV4/k_sphericalcoords.ino
+++ HexapodCodeV4/k_sphericalcoords.ino
@@ -18,34 +18,34 @@
 
 void LocalSpherical(void){//convert from global spherical angles to local spherical angles
   //Leg0, no change
-  LegLocalSpherical[0][0] = LegGlobalSpherical[0][0];//Azimuth
-  LegLocalSpherical[0][1] = LegGlobalSpherical[0][1];//Elevation
-  LegLocalSpherical[0][2] = LegGlobalSpherical[0][2];//Bank
+  leg_local_spherical[0][0] = leg_global_spherical[0][0];//Azimuth
+  leg_local_spherical[0][1] = leg_global_spherical[0][1];//Elevation
+  leg_local_spherical[0][2] = leg_global_spherical[0][2];//Bank
   //Leg1, no change
-  LegLocalSpherical[0][0] = LegGlobalSpherical[0][0];//Azimuth
-  LegLocalSpherical[0][1] = LegGlobalSpherical[0][1];//Elevation
-  LegLocalSpherical[0][2] = LegGlobalSpherical[0][2];//Bank
+  leg_local_spherical[0][0] = leg_global_spherical[0][0];//Azimuth
+  leg_local_spherical[0][1] = leg_global_spherical[0][1];//Elevation
+  leg_local_spherical[0][2] = leg_global_spherical[0][2];//Bank
   //Leg2, Azimuth +180 degrees
-  LegLocalSpherical[0][0] = LegGlobalSpherical[0][0] + PI;//Azimuth
-  LegLocalSpherical[0][1] = LegGlobalSpherical[0][1];//Elevation
-  LegLocalSpherical[0][2] = LegGlobalSpherical[0][2];//Bank
+  leg_local_spherical[0][0] = leg_global_spherical[0][0] + PI;//Azimuth
+  leg_local_spherical[0][1] = leg_global_spherical[0][1];//Elevation
+  leg_local_spherical[0][2] = leg_global_spherical[0][2];//Bank
   //Leg3, Azimuth +180 degrees
-  LegLocalSpherical[0][0] = LegGlobalSpherical[0][0] + PI;//Azimuth
-  LegLocalSpherical[0][1] = LegGlobalSpherical[0][1];//Elevation
-  LegLocalSpherical[0][2] = LegGlobalSpherical[0][2];//Bank
+  leg_local_spherical[0][0] = leg_global_spherical[0][0] + PI;//Azimuth
+  leg_local_spherical[0][1] = leg_global_spherical[0][1];//Elevation
+  leg_local_spherical[0][2] = leg_global_spherical[0][2];//Bank
 }
 
 
 
 
-void LocalSphericalCalculate(void){//Calculates new TibaRot and Tarsus based on local azimuth and elevation from LegLocalSpherical
-  //Call after calulating and updating LegDynamixels
+void LocalSphericalCalculate(void){//Calculates new TibaRot and Tarsus based on local azimuth and elevation from leg_local_spherical
+  //Call after calulating and updating leg_dynamixels
   for(int leg = 0; leg < NUM_LEGS; leg++){
-    float Azimuth = LegLocalSpherical[leg][0];
-    float Elevation = LegLocalSpherical[leg][1];
-    float Coxa = LegDynamixels[leg][0];
-    float Femur = LegDynamixels[leg][1];
-    float Tibia = LegDynamixels[leg][2];
+    float Azimuth = leg_local_spherical[leg][0];
+    float Elevation = leg_local_spherical[leg][1];
+    float Coxa = leg_dynamixels[leg][0];
+    float Femur = leg_dynamixels[leg][1];
+    float Tibia = leg_dynamixels[leg][2];
     //float IniVect[3] = {cos(coxa)*sin(femur+tibia),sin(coxa)*cos(femur+tibia),cos(femur+tibia)};//vector of the leg(x,y,z)
     Azimuth -=Coxa;//The first rotation is faster this way, no trig compared to a cartesian rotation
     float DesVect1[3] = {
@@ -55,7 +55,7 @@ void LocalSphericalCalculate(void){//Calculates new TibaRot and Tarsus based on 
 
 
     float pitch = (HALF_PI + Femur + Tibia);
-    UserSerial.println(pitch);
+    USER_SERIAL.println(pitch);
 
 
     float XZCorr[3][3] = {//Matrix to rotate point to be in proper loaction for sphericalcoords
@@ -71,12 +71,12 @@ void LocalSphericalCalculate(void){//Calculates new TibaRot and Tarsus based on 
 
 
 #if defined(UserSerialTransmit)
-    UserSerial.print("Angle x ");
-    UserSerial.println(DesVect1[0]);
-    UserSerial.print("Angle y ");
-    UserSerial.println(DesVect1[1]);
-    UserSerial.print("Angle z ");
-    UserSerial.println(DesVect1[2]);
+    USER_SERIAL.print("Angle x ");
+    USER_SERIAL.println(DesVect1[0]);
+    USER_SERIAL.print("Angle y ");
+    USER_SERIAL.println(DesVect1[1]);
+    USER_SERIAL.print("Angle z ");
+    USER_SERIAL.println(DesVect1[2]);
 #endif
 
 
@@ -85,22 +85,22 @@ void LocalSphericalCalculate(void){//Calculates new TibaRot and Tarsus based on 
     DesVect2[1] = XZCorr[1][0]*DesVect1[0] + XZCorr[1][1]*DesVect1[1] + XZCorr[1][2]*DesVect1[2];
     DesVect2[2] = XZCorr[2][0]*DesVect1[0] + XZCorr[2][1]*DesVect1[1] + XZCorr[2][2]*DesVect1[2];
 
-    LegDynamixels[leg][3] = atan2f(DesVect2[1],DesVect2[0]);//Tibia Rotation in Degrees
-    LegDynamixels[leg][4] = -(acosf(DesVect2[2]/10));//Tarsus
-    //LegDynamixels[leg][5] = LegLocalSpherical[leg][2];//wheel, Not currently implemented in hardware.
+    leg_dynamixels[leg][3] = atan2f(DesVect2[1],DesVect2[0]);//Tibia Rotation in Degrees
+    leg_dynamixels[leg][4] = -(acosf(DesVect2[2]/10));//Tarsus
+    //leg_dynamixels[leg][5] = leg_local_spherical[leg][2];//wheel, Not currently implemented in hardware.
   }
 }
 
 
-void LegPlaceSixCalculate (void){//place leg in six degrees of freedom based on LegCoordsOut and LegLocalSpherical leg -1 places all legs
+void LegPlaceSixCalculate (void){//place leg in six degrees of freedom based on LegCoordsOut and leg_local_spherical leg -1 places all legs
   //Transform the coordinates for each leg
   for(int leg = 0; leg < NUM_LEGS; leg++){
     int Xplace = LegCoordsOut[leg][0];
     int Yplace = LegCoordsOut[leg][1];
     int Zplace = LegCoordsOut[leg][2];
-    float Azimuth = LegLocalSpherical[leg][0];
-    float Elevation = LegLocalSpherical[leg][1];
-    float Bank = LegLocalSpherical[leg][2];
+    float Azimuth = leg_local_spherical[leg][0];
+    float Elevation = leg_local_spherical[leg][1];
+    float Bank = leg_local_spherical[leg][2];
 
     //Shift X,Y, and Z coordinates to compinsate for the tarsus
     float radius = TARSUS_LENGTH*cosf(Elevation);
